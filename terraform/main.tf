27765965aea49818a8bed6fe23d9018edafd6d72
Siva Bhimireddy <sivabhimireddy@Shiva-B.local>
2024-09-18 17:50:14 +0800

    Adding new account code

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "development+iot+orgtest-afttestc@mh-iot.com"
+    AccountName               = "Aft-testc"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "development+iot+orgtest-afttestc@mh-iot.com"
+    SSOUserFirstName          = "AFT"
+    SSOUserLastName           = "test"
   }
 
   account_tags = {
